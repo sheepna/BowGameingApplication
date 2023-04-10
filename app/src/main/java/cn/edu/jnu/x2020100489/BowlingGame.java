c2ed2029872fb1d66e55e70ca508008e331c20b6
yangna <yangna1943@outlook.com>
2023-04-10 16:33:45 +0800

    添加代码以通过第二次测试

--- app/src/main/java/cn/edu/jnu/x2020100489/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2020100489/BowlingGame.java
@@ -2,11 +2,12 @@ package cn.edu.jnu.x2020100489;
 
 public class BowlingGame {
     private int score=0;
-    public void roll(int i) {
+    public void roll(int pin) {
+        score+=pin;
     }
 
     public int score() {
-        score=3*10;
+
         return score;
     }
 }
