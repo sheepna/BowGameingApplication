a26b43fedca23095d66b696b766adf30d0c8d5fb
yangna <yangna1943@outlook.com>
2023-04-10 16:56:54 +0800

    第三次测试

--- app/src/main/java/cn/edu/jnu/x2020100489/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2020100489/BowlingGame.java
@@ -1,11 +1,11 @@
 package cn.edu.jnu.x2020100489;
 
 public class BowlingGame {
+
     private int score=0;
     public void roll(int pin) {
         score+=pin;
     }
-
     public int score() {
 
         return score;
--- app/src/test/java/cn/edu/jnu/x2020100489/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2020100489/BowlingGameUnitTest.java
@@ -1,5 +1,6 @@
 package cn.edu.jnu.x2020100489;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,28 +11,43 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class BowlingGameUnitTest {
+
+    private BowlingGame g;
+
+    @Before
+    public void setUp() throws Exception {
+        g = new BowlingGame();
+    }
+
     @Test
     public void FirstTest() {
-        BowlingGame g=new BowlingGame();
-        for(int i=0;i<10;i++){
-            g.roll(3);
-        }
-        for(int i=0;i<(20-10);i++){
-            g.roll(0);
-        }
-        assertEquals(30,g.score());
+        rollMany(10, 3);
+        rollMany((20 - 10), 0);
+        assertEquals(30, g.score());
     }
     @Test
     public void AFrame(){
-        BowlingGame g=new BowlingGame();
-        for (int i=0;i<16;i++){
-            g.roll(0);
-        }
+        rollMany(16, 0);
         g.roll(3);
         g.roll(1);
         g.roll(0);
         g.roll(0);
         assertEquals(4,g.score());
+    }
+    @Test
+    public void ASpare(){
+        rollMany(10,4);
+        g.roll(1);
+        g.roll(9);
+        rollMany(8,3);
+        assertEquals(77,g.score());
+    }
+
+
+    private void rollMany(int x, int pin) {
+        for (int i = 0; i< x; i++){
+            g.roll(pin);
+        }
     }
 
 }
