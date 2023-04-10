4cbe32d33f62ecd977fa2026172c38f34a5c0b16
yangna <yangna1943@outlook.com>
2023-04-10 16:58:25 +0800

    添加代码以通过第三次测试

--- app/src/main/java/cn/edu/jnu/x2020100489/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2020100489/BowlingGame.java
@@ -1,13 +1,24 @@
 package cn.edu.jnu.x2020100489;
 
 public class BowlingGame {
+    private int []everyRollPin=new int[21];
+    private int currentRoll=0;
 
-    private int score=0;
     public void roll(int pin) {
-        score+=pin;
+        everyRollPin[currentRoll]=pin;
+        currentRoll++;
     }
-    public int score() {
 
+    public int score() {
+        int score=0;
+        for (int rollIndex=0;rollIndex<everyRollPin.length;rollIndex++){
+            score+=everyRollPin[rollIndex];
+            if(rollIndex<19){
+                if(10==everyRollPin[rollIndex]+everyRollPin[rollIndex+1]){
+                    score+=everyRollPin[rollIndex+2];
+                }
+            }
+        }
         return score;
     }
 }
